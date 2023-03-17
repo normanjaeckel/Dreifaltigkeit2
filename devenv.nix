3f7939607a94df61908eb11d8271d76c2a04130e
Norman Jäckel <dreifaltigkeit@normanjaeckel.de>
2023-03-17 22:39:40 +0100

    Changed devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,26 +1,35 @@
 { pkgs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git pkgs.nixfmt ];
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
   languages.python = {
     enable = true;
     venv.enable = true;
   };
+
+  enterShell = ''
+    pip install --upgrade --requirement requirements.txt
+    pip freeze
+  '';
+
+  services.postgres = {
+    enable = true;
+  };
+
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  # packages = [ pkgs.git pkgs.nixfmt ];
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = "echo hello from $GREET";
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+  # languages.python = {
+  #   enable = true;
+  #   venv.enable = true;
+  # };
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
